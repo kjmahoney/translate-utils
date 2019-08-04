0050ccd5f39b26b237b7ed30376fb903c51755b5
Kevin Mahoney <kjmahoney13@gmail.com>
2019-08-04 16:11:46 -0400

    return translate object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import fetch from 'node-fetch'
 import 'dotenv/config'
+import pinyin from 'pinyin'
+
+const key = process.env.API_KEY
 
 const getWord = async (text, lang) => {
-    const key = process.env.API_KEY
-    console.log(key)
     const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=${lang}`
 
-    const word = await fetch(url);
-    const json = await word.json();
-    console.log(json)
+    const word = await fetch(url)
+    const json = await word.json()
 }
-getWord('hello', 'en-zh')
+
+const englishToChinese = async text => {
+    const lang = 'en-zh'
+    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=${lang}`
+
+    const word = await fetch(url)
+    const json = await word.json()
+    const chineseWord = json.text[0]
+    const pinyinArray = pinyin(chineseWord)
+
+    let newPinyinArray = []
+    pinyinArray.forEach(word => {
+        newPinyinArray.push(word[0])
+    })
+
+    const pinyinString = newPinyinArray.join('')
+
+    const translateObject = {
+        english: text,
+        chinese: chineseWord,
+        pinyin: pinyinString,
+    }
+    return translateObject
+}
+
+englishToChinese('hello')
