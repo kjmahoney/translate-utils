58e6264c8e7feaf1fbb22b667edef5961e8de8d5
Kevin Mahoney <kmahoney@theatlantic.com>
2019-08-05 10:10:21 -0400

    export translate functions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
-import fetch from 'node-fetch'
-import 'dotenv/config'
-import pinyin from 'pinyin'
+import {englishToChinese, chineseToEnglish} from './translate';
 
-const key = process.env.API_KEY
+export { englishToChinese, chineseToEnglish}
 
-const getWord = async (text, lang) => {
-    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=${lang}`
 
-    const word = await fetch(url)
-    const json = await word.json()
-}
 
-const englishToChinese = async text => {
-    const lang = 'en-zh'
-    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=${lang}`
 
-    const word = await fetch(url)
-    const json = await word.json()
-    const chineseWord = json.text[0]
-    const pinyinArray = pinyin(chineseWord)
 
-    let newPinyinArray = []
-    pinyinArray.forEach(word => {
-        newPinyinArray.push(word[0])
-    })
-
-    const pinyinString = newPinyinArray.join('')
-
-    const translateObject = {
-        english: text,
-        chinese: chineseWord,
-        pinyin: pinyinString,
-    }
-    return translateObject
-}
-
-englishToChinese('hello')
---
+++ src/translate.js
@@ -0,0 +1,48 @@
+import fetch from 'node-fetch'
+import 'dotenv/config'
+import pinyin from 'pinyin'
+
+const key = process.env.API_KEY
+
+const getPinyin = text => {
+    const pinyinArray = pinyin(text)
+    let newPinyinArray = []
+    pinyinArray.forEach(text => {
+        newPinyinArray.push(text[0])
+    })
+    return newPinyinArray.join('')
+}
+
+const englishToChinese = async text => {
+    const lang = 'en-zh'
+    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=${lang}`
+
+    const word = await fetch(url)
+    const json = await word.json()
+    const translation = json.text[0]
+
+    const translateObject = {
+        translation: translation,
+        pinyin: getPinyin(translation),
+    }
+    console.log(translateObject)
+    return translateObject
+}
+
+const chineseToEnglish = async hanzi => {
+    const lang = 'zh-en'
+    const text = encodeURI(hanzi)
+    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=${lang}`
+    const word = await fetch(url)
+    const json = await word.json()
+    const translation = json.text[0]
+
+    const translateObject = {
+        translation: translation,
+        pinyin: getPinyin(hanzi),
+    }
+    console.log(translateObject)
+    return translateObject
+}
+
+export { englishToChinese, chineseToEnglish }
