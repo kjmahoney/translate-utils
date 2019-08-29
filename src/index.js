da5d034470c5fe620d78dda03a1da3b2a4d19769
Kevin Mahoney <kmahoney@theatlantic.com>
2019-08-29 13:59:55 -0400

    fix index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { translate, getPinyin } from './translate';
-import { translationIsCorrect } from './quiz';
-import { toneIsCorrect } from './tone-quiz';
-
-export { translate, getPinyin, translationIsCorrect, toneIsCorrect };
