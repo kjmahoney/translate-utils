056466d9afc3310f37a5897f887e2a54c1ebfc97
Kevin Mahoney <kmahoney@theatlantic.com>
2019-08-30 12:12:23 -0400

    remove console.log

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import { translate, getPinyin } from './src/translate';
 import { translationIsCorrect } from './src/quiz';
 import { toneIsCorrect } from './src/tone-quiz';
 
-console.log(translate('hello'))
-
 export { translate, getPinyin, translationIsCorrect, toneIsCorrect };
